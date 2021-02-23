6fa2b186679b011a9e3bd66f5ead34da4f383edf
Alex Cotiugă <alexandru.cotiuga@xwiki.com>
2021-02-23 15:08:37 +0200

    The ldapuserimport component cannot be initialized when installing it in multiple wikis instead of on farm #17

--- application-ldapuserimport-api/src/main/java/com/xwiki/ldapuserimport/internal/LDAPUserImportConfigurationSource.java
+++ application-ldapuserimport-api/src/main/java/com/xwiki/ldapuserimport/internal/LDAPUserImportConfigurationSource.java
@@ -43,7 +43,7 @@ public class LDAPUserImportConfigurationSource extends AbstractXWikiPreferencesC
     @Override
     protected String getCacheId()
     {
-        return "configuration.ldapuserimport.wiki";
+        return getCacheKeyPrefix() + ".configuration.ldapuserimport.wiki";
     }
 
     @Override
