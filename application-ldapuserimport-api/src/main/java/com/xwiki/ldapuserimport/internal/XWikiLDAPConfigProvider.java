30211c30d69ae36a6377ede5a3abd1d2d58f71d6
Teodor Caras <trrenty@gmail.com>
2024-08-27 15:53:02 +0300

    [Misc] Initialize the LDAP configuration with the SSL settings coming from the Active Directory application

--- application-ldapuserimport-api/src/main/java/com/xwiki/ldapuserimport/internal/XWikiLDAPConfigProvider.java
+++ application-ldapuserimport-api/src/main/java/com/xwiki/ldapuserimport/internal/XWikiLDAPConfigProvider.java
@@ -19,6 +19,8 @@
  */
 package com.xwiki.ldapuserimport.internal;
 
+import java.util.Arrays;
+
 import javax.inject.Inject;
 import javax.inject.Named;
 import javax.inject.Provider;
@@ -31,7 +33,14 @@ import org.xwiki.component.manager.ComponentLookupException;
 import org.xwiki.component.manager.ComponentManager;
 import org.xwiki.configuration.ConfigurationSource;
 import org.xwiki.contrib.ldap.XWikiLDAPConfig;
+import org.xwiki.model.reference.DocumentReference;
+import org.xwiki.model.reference.SpaceReference;
 
+import com.xpn.xwiki.XWiki;
+import com.xpn.xwiki.XWikiContext;
+import com.xpn.xwiki.XWikiException;
+import com.xpn.xwiki.doc.XWikiDocument;
+import com.xpn.xwiki.objects.BaseObject;
 import com.xwiki.ldapuserimport.LDAPUserImportConfiguration;
 
 /**
@@ -56,6 +65,18 @@ public class XWikiLDAPConfigProvider implements Provider<XWikiLDAPConfig>
 
     private static final String DEFAULT_LDAP_FIELDS_MAPPING = "first_name=givenName,last_name=sn,email=mail";
 
+    private static final String LDAP_SSL = "ldap_ssl";
+
+    private static final String LDAP_SSL_KEYSTORE = "ldap_ssl.keystore";
+
+    private static final String LDAP_SSL_SECURE_PROVIDER = "ldap_ssl.secure_provider";
+
+    private static final SpaceReference AD_CODE_SPACE_REFERENCE =
+        new SpaceReference("xwiki", Arrays.asList("ActiveDirectory", "Code"));
+
+    @Inject
+    private Provider<XWikiContext> contextProvider;
+
     @Inject
     private ConfigurationSource configurationSource;
 
@@ -75,7 +96,32 @@ public class XWikiLDAPConfigProvider implements Provider<XWikiLDAPConfig>
         XWikiLDAPConfig configuration = new XWikiLDAPConfig(null, getConfigurationSource());
         setPageNameFormatter(configuration);
         setFieldMapping(configuration);
+        try {
+            setSSLProperties(configuration);
+        } catch (XWikiException e) {
+            logger.warn("Could not retrieve the SSL related configuration of the Active Directory application.");
+        }
         return configuration;
+    }
+
+    private void setSSLProperties(XWikiLDAPConfig configuration) throws XWikiException
+    {
+        DocumentReference adConfigClassRef =
+            new DocumentReference("ActiveDirectoryConfigClass", AD_CODE_SPACE_REFERENCE);
+        DocumentReference adConfigRef = new DocumentReference("ActiveDirectoryConfig", AD_CODE_SPACE_REFERENCE);
+
+        XWikiContext context = this.contextProvider.get();
+        XWiki xwiki = context.getWiki();
+        XWikiDocument adConfigDoc = xwiki.getDocument(adConfigRef, context);
+        BaseObject adConfigObj = adConfigDoc.getXObject(adConfigClassRef);
+        if (adConfigObj != null) {
+            configuration.setFinalProperty(LDAP_SSL, adConfigObj.getStringValue(LDAP_SSL));
+            configuration.setFinalProperty(LDAP_SSL_KEYSTORE, adConfigObj.getStringValue(LDAP_SSL_KEYSTORE));
+
+            String provider = adConfigObj.getStringValue(LDAP_SSL_SECURE_PROVIDER);
+            configuration.setFinalProperty(LDAP_SSL_SECURE_PROVIDER,
+                StringUtils.isNoneBlank(provider) ? provider : null);
+        }
     }
 
     private ConfigurationSource getConfigurationSource()
